9695c326434842d8781217887d7f8fdf0d4d2dd9
ishad ramzan <ishadh99@gmail.com>
2025-06-23 21:38:20 +0530

    progess by night

--- script.js
+++ script.js
@@ -2,55 +2,30 @@
 console.log("WELCOME ETCH A SKETCH!")
 
 
-const startButton = document.getElementById("start");
 const addButton = document.getElementById("add");
 const container = document.getElementById("container");
 
-addButton.disable = true;
+// addButton.addEventListener("click", () => {
 
-
-startButton.addEventListener("click", () => {
-   
-    console.log("Game Started!");
-
-    const newDiv = document.createElement("div");
-    //  // Add some text inside the new div
-    newDiv.textContent = "I'm a new div!";
-    // Optional: Add a class for styling
-    newDiv.classList.add("new-box");
-    // Append the new div to the container
-    container.appendChild(newDiv);
-
-    startButton.disabled = true;
     
-});
+// addButton.disabled = true;
 
+    for(let j = 1; j<17; j++){
 
-
-addButton.addEventListener("click", () => {
-   
     const newDiv = document.createElement("div");
-    //  // Add some text inside the new div
-    newDiv.textContent = "I'm a new div!";
-    // Optional: Add a class for styling
-    newDiv.classList.add("new-box");
-    // Append the new div to the container
+    newDiv.classList.add(`row`);
     container.appendChild(newDiv);
 
-    if (container.childElementCount == 4) {
-
-        for(i = 0;i<4;i++){
-                    console.log("inside if!")
-    const newDiv = document.createElement("div");
-    container.appendChild(newDiv);
-        newDiv.textContent = "I'm a flex test!";
-        container.classList.add("flex-column");
-
+    for(let i = 1; i<17; i++){
+        const secDiv = document.createElement("div");
+        secDiv.classList.add("second");
+        secDiv.classList.add("fade-in");
+        secDiv.textContent = `${j}`;
+        newDiv.append(secDiv);
         }
-
     }
 
-});
+// });
 
 
 
